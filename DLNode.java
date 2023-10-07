d37c589e0c80677286ce2df955ecfa68fededcf5
Nikhil Jindal <njindal2004@gmail.com>
2023-10-07 17:21:39 -0400

    Added javadoc comments

--- DLNode.java
+++ DLNode.java
@@ -1,35 +1,74 @@
+/**
+ * Class that contains the implementation of a doubly linked list node
+ * @author Nikhil Jindal
+ */
 public class DLNode <T> {
     
+    // instance field that holds the element in the node
     private T element;
+
+    // instance field that holds a pointer to the next node
     private DLNode<T> next;
+    
+    // instance field that holds a pointer to the previous node
     private DLNode<T> prev;
 
+    /**
+     * Constructor for DLNode
+     * @param element  the element to be stored in the node
+     * @param next  the pointer to the next node
+     * @param prev  the pointer to the previous node
+     */
     public DLNode(T element, DLNode<T> next, DLNode<T> prev) {
         this.element = element;
         this.next = next;
         this.prev = prev;
     }
 
+    /**
+     * Returns the element stored in the node
+     * @return the element stored in the node
+     */
     public T getElement() {
         return element;
     }
 
+    /**
+     * Sets the element stored in the node
+     * @param element  the element to be stored in the node
+     */
     public void setElement(T element) {
         this.element = element;
     }
 
+    /**
+     * Returns the pointer to the next node
+     * @return the pointer to the next node
+     */
     public DLNode<T> getNext() {
         return next;
     }
 
+    /**
+     * Sets the pointer to the next node
+     * @param next  the pointer to the next node
+     */
     public void setNext(DLNode<T> next) {
         this.next = next;
     }
 
+    /**
+     * Returns the pointer to the previous node
+     * @return the pointer to the previous node
+     */
     public DLNode<T> getPrev() {
         return prev;
     }
 
+    /**
+     * Sets the pointer to the previous node
+     * @param prev  the pointer to the previous node
+     */
     public void setPrev(DLNode<T> prev) {
         this.prev = prev;
     }
--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -1,56 +1,102 @@
+/**
+ * Class that implements a double linked list (DLL)
+ * @author Nikhil Jindal
+ */
 public class DoubleLinkedList<T> {
+    
+    // an instance field representing the head of the DLL
     private DLNode<T> head;
+
+    // an instance field representing the tail of the DLL
     private DLNode<T>tail;
 
+    // constructor
     public DoubleLinkedList() {
         head = tail = null;
     }
 
+    /**
+     * Returns the head of the DLL
+     * @return head of the DLL
+     */
     public DLNode<T> getHead() {
         return head;
     }
 
+    /**
+     * Sets the head of the DLL
+     * @param head head of the DLL
+     */
     public void setHead(DLNode<T> head) {
         this.head = head;
     }
 
+    /**
+     * Returns the tail of the DLL
+     * @return tail of the DLL
+     */
     public DLNode<T> getTail() {
         return tail;
     }
 
+    /**
+     * Sets the tail of the DLL
+     * @param tail  tail of the DLL
+     */
     public void setTail(DLNode<T> tail) {
         this.tail = tail;
     }
 
+    /**
+     * Adds a node to the end of the DLL
+     * @param node  node to be added
+     */
     public void add(DLNode<T> node) {
+        
         if (head == null) {
+            // if the list is empty
             head = tail = node;
         } else {
+            // if the list is not empty
             tail.setNext(node);
             node.setPrev(tail);
             tail = node;
         }
     }
 
+    /**
+     * Removes a node from the DLL
+     * @param node  node to be removed
+     */
     public void remove(DLNode<T> node) {
         if (node.getNext() != null) {
             if (node == head) {
+                // if the node is the head of the list
                 head = node.getNext();
             } else if (tail == node) {
+                // if the node is the tail of the list
                 tail = node.getPrev();
             } else {
+                // if the node is in the middle of the list
                 node.getNext().setPrev(node.getPrev());
                 node.getPrev().setNext(node.getNext());
             }
         }
     }
 
+    /**
+     * Swaps the successor of a node with the current node
+     * @param node  node whose successor is to be swapped
+     */
     public void successorSwap(DLNode<T> node) {
+        
+        // find the node that is inputted
         DLNode<T> current = head;
         while (current != node || current.getNext() != null) {
             current = current.getNext();
         }
 
+        // swap the successor of the current node
         if (current.getPrev() != null && current.getNext() != null && current.getNext().getNext() != null) {
             // with nodes before, after, and two after the current node
             current.getPrev().setNext(current.getNext());
